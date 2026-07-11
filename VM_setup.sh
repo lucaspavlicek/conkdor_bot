98809e0723c3cf339040860393cd0f6e0d19c4b6
Lucas pavlicek <lucas.pavlicek@gmail.com>
2026-07-11 00:59:25 -0700

    Added service file to repo.

--- VM_setup.sh
+++ VM_setup.sh
@@ -5,3 +5,7 @@ sudo apt install python3-pip -y
 sudo apt install python3-venv -y
 
 python3 -m venv venv
+
+./venv/bin/pip install -e .
+
+cp conkdor_bot.service /etc/systemd/system/conkdor_bot.service
